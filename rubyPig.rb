6531e1f440772ff1cd80f43f1ae781cb31120fb7
Avery VanKirk <averyvankirk@gmail.com>
2017-08-07 23:04:51 -0700

    connection exception handling

--- rubyPig.rb
+++ rubyPig.rb
@@ -19,44 +19,50 @@ class Pig
 		@users = 0
 	end
 
-	def listen
-
-		server = TCPServer.new(@lip,@lport)
-		Thread.start do
-			head = server.accept
-			server.close
-			@head = head
-			num = @users
-			@users = @users + 1
-			head.puts "[Server] Connected as head"
-			while line = head.gets
-				puts "head: #{line}"
-				if @tail
-					@tail.puts line
+	def listen(lip = @lip,lport = @lport)
+		begin
+			server = TCPServer.new(lip,lport)
+			Thread.start do
+				head = server.accept
+				server.close
+				@head = head
+				num = @users
+				@users = @users + 1
+				head.puts "[Server] Connected as head"
+				while line = head.gets
+					puts "head: #{line}"
+					if @tail
+						@tail.puts line
+					end
 				end
 			end
+		rescue => e
+			puts "Exception: #{@lip}:#{@lport} - #{ e.message }"
 		end
 	end
 
-	def connect
-
-		Thread.start(TCPSocket.new(@rip,@rport)) do |tail|
-			@tail = tail
-			tail.puts "[Server] Connected as tail"
-			while line = tail.gets
-				puts "tail: #{line}"
-				if @head
-					@head.puts line
+	def connect(rip = @rip, rport = @rport)
+		begin
+			Thread.start(TCPSocket.new(rip,rport)) do |tail|
+				@tail = tail
+				tail.puts "[Server] Connected as tail"
+				while line = tail.gets
+					puts "tail: #{line}"
+					if @head
+						@head.puts line
+					end
 				end
+				tail.close
 			end
-			tail.close
-		end		
+		rescue => e
+			puts "Exception: #{@rip}:#{@rport} - #{ e.message }"
+
+		end	
 	end
 
 	def about
-		puts @lip
-		puts @port
-		puts "connected: #{@head}" 
+		puts "Head: #{@lip}:#{@lport} - connected: #{@head}"
+		puts "Tail: #{@rip}:#{@rport} - connected: #{@tail}"
 	end
 end
 
@@ -64,9 +70,18 @@ if __FILE__ == $0
 	p = Pig.new()
 	p.listen
 	p.connect
-	#loop to keep threads alive 
-	puts "Ay"
+	#p.about
+
 	while true
-		sleep(0.1)
+		print ">"
+		input = gets.split(" ")
+		puts "input was #{input}"
+		if input[0] == "connect" and input.length == 3
+			p.connect(input[1],input[2])
+		elsif input[0] == "listen" and input.length == 3
+			p.listen(input[1],input[2])		
+		end		
 	end
 end
+
+
